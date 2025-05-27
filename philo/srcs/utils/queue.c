c31da9dd6beb3ee051f3b95fb0d2b8bc5a6b2103
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-05-27 12:34:27 +0900

    テストコード追加

--- philo/srcs/utils/queue.c
+++ philo/srcs/utils/queue.c
@@ -6,37 +6,11 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 18:55:40 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/27 11:50:59 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/27 12:34:12 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "queue.h"
-
-void	queue_init(t_queue *q)
-{
-	q->head = NULL;
-	q->tail = NULL;
-	pthread_mutex_init(&q->mutex, NULL);
-	q->enqueue = _enqueue;
-	q->dequeue = _dequeue;
-}
-
-void	queue_clear(t_queue **q)
-{
-	t_node	*curr;
-	t_node	*next;
-
-	if (!q || !*q)
-		return ;
-	curr = (*q)->head;
-	while (curr)
-	{
-		next = curr->next;
-		free(curr);
-		curr = next;
-	}
-	*q = NULL;
-}
 
 /**
  * @return true     :SUCCESS
@@ -86,4 +60,30 @@ static t_msg	*_dequeue(t_queue *self)
 	}
 	pthread_mutex_unlock(&self->mutex);
 	return (msg);
+}
+
+void	queue_init(t_queue *q)
+{
+	q->head = NULL;
+	q->tail = NULL;
+	pthread_mutex_init(&q->mutex, NULL);
+	q->enqueue = _enqueue;
+	q->dequeue = _dequeue;
+}
+
+void	queue_clear(t_queue **q)
+{
+	t_node	*curr;
+	t_node	*next;
+
+	if (!q || !*q)
+		return ;
+	curr = (*q)->head;
+	while (curr)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+	*q = NULL;
 }
--- philo/tests/utils/test_queue.c
+++ philo/tests/utils/test_queue.c
@@ -6,16 +6,17 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 23:40:09 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/05/26 23:51:46 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/05/27 12:27:03 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "queue.h"
 #include "unity.h"
 
-t_queue *q;
+t_queue *q = NULL;
 
 void setUp(void) {
+    q = malloc(sizeof(t_queue));
     queue_init(q);
 }
 
@@ -23,12 +24,26 @@ void tearDown(void) {
     queue_clear(&q);
 }
 
-void test_1(void) {
-    TEST_ASSERT_EQUAL_INT(4, 2 + 2);
+void    test_push_and_pop(void) {
+    t_msg msg1;
+    t_msg *msg2;
+
+    msg1 = (t_msg){.type=UPDATE, .args="NULL", .sender=NULL};
+
+    q->enqueue(q, &msg1);
+    msg2 = q->dequeue(q);
+    TEST_ASSERT_EQUAL_PTR(&msg1, msg2);
 }
 
-int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_1);
-    return UNITY_END();
+void    test_dequeue_is_empty(void)
+{
+    t_msg   *msg;
+    
+    msg = q->dequeue(q);
+    TEST_ASSERT_NULL(msg);
+}
+
+void test_queue(void) {
+    RUN_TEST(test_push_and_pop);
+    RUN_TEST(test_dequeue_is_empty);
 }
