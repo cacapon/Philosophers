09ae155b7d20726005a91ed634e78ea45e0594b4
cacapon <cacapon.project@gmail.com>
2025-06-03 19:13:00 +0900

    add philo_calloc

---
+++ philo/inc/utils/philo_calloc.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_calloc.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 19:05:32 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/06/03 19:07:02 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_CALLOC_H
+# define PHILO_CALLOC_H
+
+# include <stdlib.h>
+# include <stdint.h>
+
+void	*ft_actor_calloc(size_t count, size_t size);
+
+#endif
---
+++ philo/srcs/utils/philo_calloc.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_calloc.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 19:04:40 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/06/03 19:11:36 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_calloc.h"
+
+static void	*ft_actor_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	if (!b)
+		return (b);
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
+}
+
+void	*ft_actor_calloc(size_t count, size_t size)
+{
+	void	*_block;
+
+	if (count == 0 || size == 0)
+		return (malloc(0));
+	if (size > SIZE_MAX / count)
+		return (NULL);
+	_block = malloc(count * size);
+	if (!_block)
+		return (NULL);
+	ft_actor_memset(_block, 0, count * size);
+	return (_block);
+}
