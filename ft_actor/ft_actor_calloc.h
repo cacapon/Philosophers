aa47f102fdbbdffb51e51d263fbaf0643c4769b2
cacapon <cacapon.project@gmail.com>
2025-06-16 19:59:12 +0900

    add ft_actor_calloc

---
+++ ft_actor/ft_actor_calloc.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_actor_calloc.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 19:05:32 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/06/16 19:58:37 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ACTOR_CALLOC_H
+# define FT_ACTOR_CALLOC_H
+
+# include <stdlib.h>
+# include <stdint.h>
+
+void	*ft_actor_calloc(size_t count, size_t size);
+
+#endif
