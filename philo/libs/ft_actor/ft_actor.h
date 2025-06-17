8655166e261661bf60c48e972d8594d307cd0d90
cacapon <cacapon.project@gmail.com>
2025-06-17 22:12:36 +0900

    add ft_actor_set_parent

--- philo/libs/ft_actor/ft_actor.h
+++ philo/libs/ft_actor/ft_actor.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 18:09:06 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/16 22:13:12 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/17 22:10:39 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ typedef struct s_ft_actor
 	pthread_t				thread;
 	t_ft_queue				*inbox;
 	void					*ref;
+	void					*parent;
 	const t_ft_actor_vtable	*v;
 	bool					(*tell)(t_ft_actor * self, t_ft_msg * msg);
 	bool					is_running;
@@ -42,6 +43,7 @@ t_ft_actor	*ft_actor_new(void *ref);
 void		ft_actor_del(t_ft_actor **actor);
 void		ft_actor_start(t_ft_actor *actor);
 void		ft_actor_stop(t_ft_actor *actor);
+void		ft_actor_set_parent(t_ft_actor *actor, void *parent);
 bool		_ft_actor_tell(t_ft_actor *self, t_ft_msg *msg);
 
 #endif
