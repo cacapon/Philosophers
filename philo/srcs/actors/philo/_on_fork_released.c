c7c64d387eb13546b1aac06e5e8e55f2655cb088
cacapon <cacapon.project@gmail.com>
2025-06-21 23:15:52 +0900

    fix if has LR fork

--- philo/srcs/actors/philo/_on_fork_released.c
+++ philo/srcs/actors/philo/_on_fork_released.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:32:30 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/21 21:41:30 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/21 23:07:34 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	_on_fork_released(t_philo_actor *self, t_ft_actor *sender)
 		self->has_l_fork = false;
 	if (sender == self->r_fork)
 		self->has_r_fork = false;
-	if (!(self->has_l_fork && self->has_r_fork))
+	if (!self->has_l_fork && !self->has_r_fork)
 	{
 		self->sts = PHILO_STS_SLEEPING;
 		self->sv->tell(self->sv, msg_new(PHILO_EAT_DONE, NULL, NULL));
