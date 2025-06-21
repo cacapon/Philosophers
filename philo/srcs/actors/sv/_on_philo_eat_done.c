bec5ab476839577d5a7e19a952b9c12f25f3ad74
cacapon <cacapon.project@gmail.com>
2025-06-21 22:15:20 +0900

    fix grant_eat

--- philo/srcs/actors/sv/_on_philo_eat_done.c
+++ philo/srcs/actors/sv/_on_philo_eat_done.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 21:37:09 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/21 21:47:37 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/21 22:13:19 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,12 @@
 
 static void	_advance_to_next_phase(t_sv_actor *self)
 {
-	size_t		i;
-	t_ft_actor	*philo;
-
 	self->prop->philo_done_count++;
 	if (self->prop->philo_done_count >= self->prop->send_ptn->col)
 	{
 		self->prop->philo_done_count = 0;
 		self->prop->ptn_i = (self->prop->ptn_i + 1) % self->prop->send_ptn->row;
-		i = 0;
-		while (i < self->prop->send_ptn->col)
-		{
-			philo = self->prop->philos_ref[i++];
-			philo->tell(philo, msg_new(GRANT_EAT, NULL, NULL));
-		}
+		_send_grant_eat(self);
 	}
 }
 
