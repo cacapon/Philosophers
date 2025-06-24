d5b4c67cc233c45a20ffc2a851446f6a65312549
cacapon <cacapon.project@gmail.com>
2025-06-24 22:45:10 +0900

    add is_someone_died

--- philo/srcs/actors/monitor/monitor_on_receive.c
+++ philo/srcs/actors/monitor/monitor_on_receive.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:11:18 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/21 18:58:19 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/24 22:42:59 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 bool	monitor_on_receive(t_ft_actor *self, t_ft_msg *msg)
 {
+	t_monitor_actor	*monitor;
+
 	if (!self || !msg)
+		return (true);
+	monitor = self->ref;
+	if (monitor->is_someone_died)
 		return (true);
 	if (MONITOR_MSG_START < msg->type && msg->type < MONITOR_MSG_END)
 		_show_monitor(msg);
+	if (msg->type == MONITOR_DIED)
+		monitor->is_someone_died = true;
 	return (true);
 }
