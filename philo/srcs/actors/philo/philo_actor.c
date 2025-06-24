93ab9d35617dfb7a004bc3838c2f33e50843d608
cacapon <cacapon.project@gmail.com>
2025-06-24 14:11:48 +0900

    init philo->last_update_time

--- philo/srcs/actors/philo/philo_actor.c
+++ philo/srcs/actors/philo/philo_actor.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:32:30 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/24 13:15:37 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/24 14:10:57 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ t_philo_actor	*philo_actor_new(size_t no, t_main_args args)
 	philo->max_eat = args.time_to_eat;
 	philo->max_slp = args.time_to_sleep;
 	philo->max_eat_count = args.number_of_times_each_philosopher_must_eat;
+	gettimeofday(&philo->last_update_time, NULL);
 	return (philo);
 }
