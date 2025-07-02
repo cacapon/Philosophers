5206958690286ea11d5867613165eca3faa2687e
cacapon <cacapon.project@gmail.com>
2025-07-02 14:32:51 +0900

    Revert "fix norm"
    This reverts commit 58d59ff7ef40ff16a2cb07d7efb0e7370ca989a6.

--- philo/inc/non_actors/fork.h
+++ philo/inc/non_actors/fork.h
@@ -6,14 +6,14 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:28:12 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/30 18:39:33 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/06/30 18:35:46 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORK_H
 # define FORK_H
 
-# include <pthread.h>
+#include <pthread.h>
 
 typedef struct s_fork	t_fork;
 
