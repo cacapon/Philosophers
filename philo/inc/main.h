78680f52216b0a24b2bf3ec83b4a21e1d965955e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-07-03 12:57:24 +0900

    fix error msg

--- philo/inc/main.h
+++ philo/inc/main.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 19:04:01 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/29 13:11:00 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/07/03 12:56:24 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@
 # define USAGE_STR "usage: ./philo <number_of_philosophers> \
 <time_to_die> <time_to_eat> <time_to_sleep> \
 [number_of_times_each_philosopher_must_eat]\n"
-# define ARGS_ERR_STR "Error: num_of_philos or time_to_X must be at least 1.\n"
+# define ERR_STR1 "numbernumber_of_philosophers\t\t\t[1 <= x <= 200    ]"
+# define ERR_STR2 "time_to_die\t\t\t\t\t[1 <= x <= INT_MAX]"
+# define ERR_STR3 "time_to_eat\t\t\t\t\t[1 <= x <= INT_MAX]"
+# define ERR_STR4 "time_to_sleep\t\t\t\t\t[1 <= x <= INT_MAX]"
+# define ERR_STR5 "number_of_times_each_philosopher_must_eat\t\
+[1 <= x <= INT_MAX]"
 
 # include <stddef.h>
 # include <stdio.h>
+# include <limits.h>
 # include "ft_actor.h"
 # include "args.h"
 # include "system.h"
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,24 +6,58 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:47:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/06/29 13:15:26 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/07/03 12:56:57 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-static void	_print_args_err(t_main_args args)
+static void	_print_err(int argc, char **argv)
 {
-	int	must_eat;
+	printf("%s", USAGE_STR);
+	printf("Error: invalid argument\n");
+	printf("\t%s:%s\n", ERR_STR1, argv[1]);
+	printf("\t%s:%s\n", ERR_STR2, argv[2]);
+	printf("\t%s:%s\n", ERR_STR3, argv[3]);
+	printf("\t%s:%s\n", ERR_STR4, argv[4]);
+	if (argc == 6)
+		printf("\t%s:%s\n", ERR_STR5, argv[5]);
+}
 
-	must_eat = args.number_of_times_each_philosopher_must_eat;
-	printf("%s", ARGS_ERR_STR);
-	printf("args:\n");
-	printf("\tnumber_of_philosophers:%d\n", args.num_of_philos);
-	printf("\ttime_to_die:%d\n", args.time_to_die);
-	printf("\ttime_to_eat:%d\n", args.time_to_eat);
-	printf("\ttime_to_sleep:%d\n", args.time_to_sleep);
-	printf("\tnumber_of_times_each_philosopher_must_eat:%d\n", must_eat);
+static bool	_isnum(char *str)
+{
+	while (*str)
+	{
+		if (!('0' <= *str && *str <= '9'))
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+static bool	_in_range(int val, int min, int max)
+{
+	if (min > max)
+		return (false);
+	return (min <= val && val <= max);
+}
+
+static bool	_validate_args(int ac, char **av)
+{
+	if (!(ac == 5 || ac == 6))
+		return (false);
+	if (!_isnum(av[1]) || !_in_range(philo_atoi(av[1]), 1, 200))
+		return (false);
+	if (!_isnum(av[2]) || !_in_range(philo_atoi(av[2]), 1, INT_MAX))
+		return (false);
+	if (!_isnum(av[3]) || !_in_range(philo_atoi(av[3]), 1, INT_MAX))
+		return (false);
+	if (!_isnum(av[4]) || !_in_range(philo_atoi(av[4]), 1, INT_MAX))
+		return (false);
+	if (ac == 6)
+		if (!_isnum(av[5]) || !_in_range(philo_atoi(av[5]), 1, INT_MAX))
+			return (false);
+	return (true);
 }
 
 int	main(int argc, char **argv)
@@ -31,8 +65,8 @@ int	main(int argc, char **argv)
 	t_main_args	args;
 	t_system	*sys;
 
-	if (!(argc == 5 || argc == 6))
-		return (printf("%s", USAGE_STR), 1);
+	if (!_validate_args(argc, argv))
+		return (_print_err(argc, argv), 1);
 	args.num_of_philos = (int)philo_atoi(argv[1]);
 	args.time_to_die = (int)philo_atoi(argv[2]);
 	args.time_to_eat = (int)philo_atoi(argv[3]);
@@ -41,11 +75,6 @@ int	main(int argc, char **argv)
 		args.number_of_times_each_philosopher_must_eat = philo_atoi(argv[5]);
 	else
 		args.number_of_times_each_philosopher_must_eat = -1;
-	if (args.num_of_philos <= 0
-		|| args.time_to_die <= 0
-		|| args.time_to_eat <= 0
-		|| args.time_to_sleep <= 0)
-		return (_print_args_err(args), 1);
 	sys = system_new(args);
 	system_run(sys);
 	system_del(&sys, args.num_of_philos);
