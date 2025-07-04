06f332fcc68cb42e371f7b3a234b123648fb7906
cacapon <cacapon.project@gmail.com>
2025-07-04 19:58:14 +0900

    fix norm

--- philo/srcs/actors/fork/_set_fork_sts.c
+++ philo/srcs/actors/fork/_set_fork_sts.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 19:49:24 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/04 19:53:20 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/07/04 19:58:06 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	_set_fork_sts(t_fork_actor *self, t_fork_sts sts)
 {
 	pthread_mutex_lock(&self->mutex);
-	self->sts = sts;	
+	self->sts = sts;
 	pthread_mutex_unlock(&self->mutex);
 }
